96647bdf686f6703b04758c319973be3c3f898ff
Dan Bax <Dan.D.Bax@gmail.com>
2022-12-30 13:09:50 -0600

    Setup Styles Sheet and started work on  calculator

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const path = require('path');
 app.set('views', path.join(__dirname,'views'));
 app.set('view engine', 'ejs');
 
+app.get('/', (req, res) => {
+  res.render('index');
+})
+
 app.listen(3000,() =>{
   console.log('APP IS LISTENING ON PORT 3000!')
 })
